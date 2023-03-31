7d2a73d29b9f0b20e0103635ec9798fb9c85d00a
Arturo Ramirez <jiamafox@gmail.com>
2023-03-31 00:21:44 -0300

    resolviendo deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,4 @@
-//const { defineConfig } = require('vite')
 import { defineConfig } from 'vite'
-
-/* module.exports = defineConfig({
-  build: {
-    rollupOptions: {
-      input: ["./index.html", "./views/cart.html", "./views/detail.html"]
-    }
-  }
-}) */
-
 export default defineConfig({
   build: {
     rollupOptions: {
